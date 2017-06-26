3c64448d19db32af41deedb9e731faa0d1891967
willshepp28 <willsheppard28@outlook.com>
2017-06-25 20:16:51 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,37 +1,61 @@
+// app.js
+
+// BASE SETUP
+// ==============================================
+
 var express = require('express');
 var mustacheExpress = require('mustache-express');
+var bodyParser = require('body-parser');
+var handlebars = require('express-handlebars');
 var path = require('path');
 var data = require('./data');
 var users = data.users;
-console.log(users.name);
-console.log(users[0]);
+
+for(let i =0; i < users.length; i++) {
+    console.log([i]);
+    console.log(users[i].address.city);
+    console.log(users[i].address.country);
+    console.log('__________________')
+}
+
 var app = express();
 
-// define the port to run on 
-var port = process.env.PORT || 3000; // on our machine obviously 3000 dude
-app.use('/assets',express.static(path.join(__dirname, 'views')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/assets',express.static(path.join(__dirname, '/public')));
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 
-app.engine('mustache', mustacheExpress());
-
-
-app.set('view engine', 'mustache');
-app.set('views', __dirname + '/views');
 
 
 app.get('/', function(req,res) {
-    res.render('index', {
-            users
-
-    });
-
+    res.render('home', { users });
 });
 
-app.get('/person/:id', function(req,res) {
-    res.render('person', {id: req.params.id});
+app.get('/profilework/:id', function(req,res){
+    var ID = req.params.id;
+     var user = users[ID - 1];
+    res.render('profilework', {user, layout: false});
+});
+
+app.get('/profilelook/:id', function(req,res) {
+    var ID = req.params.id;
+    var user = users[ID - 1];
+    res.render('profilelook', {user, layout: false});
+    
+ 
 });
 
 
-app.listen(port, function(){
-    console.log('listening on port: ' + port);
+
+
+
+
+// START THE SERVER
+// ==============================================
+
+app.listen(8000, function() {
+    console.log('Listening on port 3000');
 });
